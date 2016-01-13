4088a0247e1d2f0c42a6ffb9874661b1252e7329
Alvurion <alvurion@gmail.com>
2016-01-13 16:45:28 +0100

    Funcionalidad para escuchar los ficheros de audio.

--- app/src/main/java/tta/ehu/eus/apptta/ContentActivity.java
+++ app/src/main/java/tta/ehu/eus/apptta/ContentActivity.java
@@ -1,6 +1,7 @@
 package tta.ehu.eus.apptta;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -16,9 +17,12 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
+
 import tta.ehu.eus.apptta.Modelo.Frase;
 import tta.ehu.eus.apptta.Modelo.Usuario;
 import tta.ehu.eus.apptta.Presentacion.Data;
+import tta.ehu.eus.apptta.Vista.AudioPlayer;
 
 
 public class ContentActivity extends AppCompatActivity {
@@ -63,9 +67,7 @@ public class ContentActivity extends AppCompatActivity {
 
     }
 
-    public void play(View view){
-        Toast.makeText(this,R.string.sinImplementar,Toast.LENGTH_SHORT).show();
-    }
+
 
     public View crearCardView(Frase frase){
         CardView card= new CardView(new ContextThemeWrapper(ContentActivity.this,R.style.CardViewStyle),null,0);
@@ -107,6 +109,32 @@ public class ContentActivity extends AppCompatActivity {
         cardInner.addView(tvAr);
 
         return card;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public void play(View view){
+        Uri uri = Uri.parse("http://s1.vocaroo.com/media/download_temp/Vocaroo_s1oeSrDz6rCV.mp3");
+        LinearLayout linear = (LinearLayout)findViewById(R.id.LinearContent);
+        AudioPlayer audioPlayer = new AudioPlayer(linear);
+        try {
+            audioPlayer.setAudioUri(uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
